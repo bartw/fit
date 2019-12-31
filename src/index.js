41e66df94517905ac18707f5423140786b18ed8f
Bart Wijnants <bart.w@sparkcentral.com>
2019-12-31 09:57:55 +0100

    auth flow

--- src/index.js
+++ src/index.js
@@ -1,7 +1,46 @@
 const express = require("express");
+const axios = require("axios");
+const qs = require("qs");
+
+const port = process.env.PORT || 3000;
+const flowId = process.env.FLOW_ID || "";
+const flowSecret = process.env.FLOW_SECRET || "";
 
 const app = express();
-const port = process.env.PORT || 3000
+
+app.get("/auth", (req, res) =>
+  res.redirect(
+    `https://flow.polar.com/oauth2/authorization?response_type=code&scope=accesslink.read_all&client_id=${flowId}`
+  )
+);
+
+app.get("/authcallback", async (req, res) => {
+  const { code } = req.query;
+
+  if (!code || !code.length) {
+    return res.status(400).send({ error: "Bad Request" });
+  }
+
+  const data = { grant_type: "authorization_code", code };
+
+  const buffer = new Buffer(`${flowId}:${flowSecret}`);
+  const base64 = buffer.toString("base64");
+
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${base64}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "application/json;charset=UTF-8"
+    },
+    data: qs.stringify(data),
+    url: "https://polarremote.com/v2/oauth2/token"
+  };
+
+  const response = await axios(options);
+
+  return res.send(response);
+});
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
