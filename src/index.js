00fda5c4369212be3f88db09f3e8a269c968125d
Bart Wijnants <bart.w@sparkcentral.com>
2019-12-31 10:25:09 +0100

    only return data

--- src/index.js
+++ src/index.js
@@ -21,30 +21,28 @@ app.get("/authcallback", async (req, res) => {
     return res.status(400).send({ error: "Bad Request" });
   }
 
-  return res.send(code);
+  const data = { grant_type: "authorization_code", code };
 
-  // const data = { grant_type: "authorization_code", code };
+  const buffer = new Buffer(`${flowId}:${flowSecret}`);
+  const base64 = buffer.toString("base64");
 
-  // const buffer = new Buffer(`${flowId}:${flowSecret}`);
-  // const base64 = buffer.toString("base64");
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${base64}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "application/json;charset=UTF-8"
+    },
+    data: qs.stringify(data),
+    url: "https://polarremote.com/v2/oauth2/token"
+  };
 
-  // const options = {
-  //   method: "POST",
-  //   headers: {
-  //     Authorization: `Basic ${base64}`,
-  //     "Content-Type": "application/x-www-form-urlencoded",
-  //     Accept: "application/json;charset=UTF-8"
-  //   },
-  //   data: qs.stringify(data),
-  //   url: "https://polarremote.com/v2/oauth2/token"
-  // };
-
-  // try {
-  //   const response = await axios(options);
-  //   return res.send(response);
-  // } catch (error) {
-  //   return res.status(500).send({ error });
-  // }
+  try {
+    const response = await axios(options);
+    return res.send(response.data);
+  } catch (error) {
+    return res.status(500).send({ error: 'Internal Server Error' });
+  }
 });
 
 app.get("/", (req, res) => res.send("Hello World!"));
